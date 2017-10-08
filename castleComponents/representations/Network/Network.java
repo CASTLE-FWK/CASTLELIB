4b814f7e514ed2dcc43654f7682a85e7ea2efecb
Lachlan Birdsey <lachlan.birdsey@adelaide.edu.au>
2017-10-08 16:13:32 +1030

    fixing the network representation

--- castleComponents/representations/Network/Network.java
+++ castleComponents/representations/Network/Network.java
@@ -10,27 +10,26 @@ import castleComponents.representations.Representation;
 public class Network<E> implements Representation<E> {
 
 	ArrayList<E> allEntities;
-	HashMap<String, NetworkNode> nodes;
+	HashMap<String, NetworkNode<E>> nodes;
 	ArrayList<NetworkEdge> edges;
 	
 	public Network(){
 		allEntities = new ArrayList<E>();
-		nodes = new HashMap<String, NetworkNode>();
+		nodes = new HashMap<String, NetworkNode<E>>();
 		edges = new ArrayList<NetworkEdge>();
 	}
 	
 	public void addNode(E e, String id){
 		allEntities.add(e);
-		nodes.put(id, new NetworkNode(e));
+		nodes.put(id, new NetworkNode<E>(e));
 	}
  
-	
 	@Override
 	public List<E> getEntities() {
 		return allEntities;
 	}
 	
-	public NetworkNode findNode(String name){
+	public NetworkNode<E> findNode(String name){
 		return nodes.get(name);
 	}
 	
@@ -41,49 +40,41 @@ public class Network<E> implements Representation<E> {
 	}
 
 	public boolean addEntity(E e) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean removeEntity(E e) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean removeEntityByID(String id) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean initialize(Object... objects) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean initializeEntity(Object... objects) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean initializeEntities(Object... objects) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean addEntities(List<E> es) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean addEntity(E e, Vector2 p) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
