d79021bf03018f9d59896c08c8062fcbaedc01bd
Lachlan Birdsey <lachlan.birdsey@adelaide.edu.au>
2018-01-17 12:54:58 +1030

    StreamingToGephi

---
+++ castleComponents/representations/MapGraph/StreamToGephi.java
@@ -0,0 +1,29 @@
+package castleComponents.representations.MapGraph;
+
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class StreamToGephi {
+
+	//http://localhost:8080/workspace0?
+	public String GEPHI_LOCATION = "";
+	public StreamToGephi(String location) {
+		GEPHI_LOCATION = location;
+	}
+	
+	public void sendAction(String action) throws Exception {
+		OutputStreamWriter writer = null;
+		URL url = new URL("http://localhost:8080/workspace1?operation=updateGraph");
+		URLConnection conn = url.openConnection();
+		conn.setDoOutput(true);
+		System.out.println("action: "+action);
+		writer = new OutputStreamWriter(conn.getOutputStream());
+		writer.write(action);
+		writer.flush();
+		
+		conn.getInputStream();
+		
+	}
+	
+}
